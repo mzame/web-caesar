2d750c54e832f033604c43690aea1998fac7fd18
Madelyn Zamenski <linuxfanatic@tutanota.com>
2018-09-24 19:03:43 -0500

    Caesar.py

--- caesar.py
+++ caesar.py
@@ -1,14 +1,19 @@
-from flask import Flask, request
-from caesar import rotate_string
-from flask import Flask
+def rotate_character(char, rot):
+    if char.isupper():              
+        position = ord('A')
+    elif char.islower():
+        position = ord('a')
+    else:
+        return char                 
+    order = ord(char) - position          
+    table = (order + rot) % 26 + position  
 
-app = Flask(__name__)
-app.config['DEBUG'] = True
-
-@app.route("/")
-def index():
-    return "Hello World"
-
-def encrypt("/")
-
-app.run()
+    return chr(table)  
+    
+def rotate_string(text, rot):
+    new_text = ''
+    for rotation in text:
+        new_letters = rotate_character(rotation, rot)
+        new_text += new_letters
+        #print(new_text)
+    return new_text
